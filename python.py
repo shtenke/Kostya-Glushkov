cf301c9e59bd39d40fdeb3067286a20878538c54
shtenke <143934133+shtenke@users.noreply.github.com>
2023-10-01 17:24:29 +0300

    Update python.py

--- python.py
+++ python.py
@@ -3,13 +3,26 @@ from discord.ext import commands
 from bot_logic import gen_pass
 from bot_logic import invert
 from random import choice
+from bot_logic import get_duck_image_url
+import nacl
+import youtube_dl
+import os
+import requests 
+from random import choice
+
+
 
 password_help = '!password (lenght)'
 hello_help = '!hello'
 bye_help = '!bye'
 heh_help = '!heh (lenght)'
 inv_help = '!inv (text)'
-    
+print(os.listdir('images'))
+
+file_path = "deutch.mp3"
+full_path = os.path.abspath(file_path)
+print(full_path)
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -34,17 +47,72 @@ async def bye(ctx):
 @bot.command() 
 async def inv(ctx, text):
     await ctx.send(invert(text))
+
+@bot.command() 
+async def mem(ctx):
+    with open('images/mem1.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
     
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
+async def ran_mem(ctx):
+    ran_mem = choice(os.listdir('images'))
+    with open(f'images/{ran_mem}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture) 
+           
+@bot.command()
 async def help_mes(ctx):
     help_text = f"{password_help}\n{hello_help}\n{bye_help}\n{heh_help}\n{inv_help}"
     await ctx.send(help_text)
+    
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+    
+    
+    
+    
+    
+    
+    
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+    
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+    
+    
+    
+@bot.command()
+async def play(ctx, file_path = full_path):
+    if not ctx.voice_client:
+        await ctx.invoke(join)
 
-bot.run("MTE1Mjg5OTk1Nzk3Njc0ODE1Mg.GzqEln.AVfgbVm_MevRiqFnPOalKRhtfQOEpb7-QVZUrc")
+    source = discord.FFmpegPCMAudio(file_path)
+    ctx.voice_client.play(source)
+    
+@bot.command()
+async def pause(ctx):
+    ctx.voice_client.pause()
+    
+@bot.command()
+async def resume(ctx):
+    ctx.voice_client.resume()
+    
+@bot.command()
+async def stop(ctx):
+    ctx.voice_client.stop()
+
+bot.run("token")
 
 
 
